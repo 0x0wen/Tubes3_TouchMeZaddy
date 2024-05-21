30c95922e0494b077e1c1f514a40f9cb2e1dee5c
zaidanav <13522146@std.stei.itb.ac.id>
2024-05-21 20:05:31 +0700

    default null db

--- src/TouchMeZaddy.Core/Models.cs
+++ src/TouchMeZaddy.Core/Models.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel;
 using SQLite;
 
 [Table("Biodata")]
@@ -6,35 +7,46 @@ public class Biodata
 {
     [PrimaryKey]
     [MaxLength(16)]
+    [NotNull]
     public string NIK { get; set; }
     
     [MaxLength(100)]
+    [DefaultValue(null)]
     public string Nama { get; set; }
     
     [MaxLength(50)]
+    [DefaultValue(null)]
     public string TempatLahir { get; set; }
     
+    [DefaultValue(null)]
     public DateTime? TanggalLahir { get; set; }
     
     [MaxLength(10)]
+    [DefaultValue(null)]
     public string JenisKelamin { get; set; }
     
     [MaxLength(5)]
+    [DefaultValue(null)]
     public string GolonganDarah { get; set; }
     
     [MaxLength(255)]
+    [DefaultValue(null)]
     public string Alamat { get; set; }
     
     [MaxLength(50)]
+    [DefaultValue(null)]
     public string Agama { get; set; }
     
     [MaxLength(20)]
+    [DefaultValue(null)]
     public string StatusPerkawinan { get; set; }
     
     [MaxLength(100)]
+    [DefaultValue(null)]
     public string Pekerjaan { get; set; }
     
     [MaxLength(50)]
+    [DefaultValue(null)]
     public string Kewarganegaraan { get; set; }
 }
 
@@ -44,5 +56,6 @@ public class SidikJari
     public string BerkasCitra { get; set; }
     
     [MaxLength(100)]
+    [DefaultValue(null)]
     public string Nama { get; set; }
 }
